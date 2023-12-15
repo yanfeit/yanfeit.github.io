9c4c8f2c27de5e2432575ac6222a85341faf62b9
Yanfei Tang <yanfeit89@gmail.com>
2023-12-15 16:19:09 +0800

    Update bibtexstrgen.js

--- assets/2023/bibtexstrgen.js
+++ assets/2023/bibtexstrgen.js
@@ -1,5 +1,17 @@
 const generateButton = document.getElementById('generate-button');
 const outputField = document.getElementById('output-field');
+const stopWords = ["a", "and", "of", "the", "during"];
+const keywordCount = 7;
+
+function removeStopWords(words, stopWords) {
+    return words.filter(word => !stopWords.includes(word.toLowerCase()) && word !== "");
+}
+
+// Function to pick first N words without stop words
+function firstNWordsWithoutStopWords(strArray, count, stopWords) {
+    const cleanArray = removeStopWords(strArray, stopWords);
+    return cleanArray.slice(0, Math.min(count, cleanArray.length)); // Ensure we don't go beyond array length
+}
 
 generateButton.addEventListener('click', function () {
     const bibtexInput = document.getElementById('bibtex-input').value;
@@ -28,10 +40,12 @@ generateButton.addEventListener('click', function () {
     console.log(journalMatch[1]);
 
     let journalShortcut = "";
+    let journalShortcutfull = "";
     if (journalMatch) {
         const journalTitleWords = journalMatch[1].trim().split(/\s+/);
         for (const word of journalTitleWords) {
             journalShortcut += word.substr(0, 3).toUpperCase(); // Extract first letter of each word
+            journalShortcutfull += word
         }
     }
 
@@ -42,12 +56,14 @@ generateButton.addEventListener('click', function () {
     if (titleMatch) {
         title = titleMatch[1].replace(/,/g, "."); // Replace commas with periods
     }
-    const titleWords = title.split(/\W+/);
+    let titleWords = title.split(/\W+/);
+
+    titleWords = firstNWordsWithoutStopWords(titleWords, keywordCount, stopWords);
 
     const finalTitle = titleWords.join("."); // Join words with periods
 
     // Generate the output string
-    const outputString = `${authorLastName}${year}.${journalShortcut}.${finalTitle}`;
+    const outputString = `${authorLastName}${year}.${journalShortcut}.${finalTitle}` + ` \n ` + `${authorLastName} et al., ${journalShortcutfull} (${year})`;
 
     // Update the output field
     outputField.textContent = outputString;
